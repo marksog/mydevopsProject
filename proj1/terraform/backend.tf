00dc00e8a6e8e47631f4ea07fafd95db15db691e
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2025-03-28 17:48:38 -0400

    updating bucket name

--- proj1/terraform/backend.tf
+++ proj1/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "itsjustanotherbucketforpersonalprojects"
+    bucket = "small-fufu-plenti-kati-kati"
     key    = "terraform-vpc/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
